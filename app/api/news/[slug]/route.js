b72d386224006a9f791a8fb5eae710f2719f9ddf
Md. Shoriful Islam <sisazid182713@gmail.com>
2026-02-03 10:09:03 +0600

    feat: title and description column update api added

--- app/api/news/[slug]/route.js
+++ app/api/news/[slug]/route.js
@@ -1,4 +1,4 @@
-import { getNewsBySlug } from "@/lib/news"
+import { getAllNews, getNewsBySlug, saveNews } from "@/lib/news"
 
 export function GET(req, { params }) {
     const news = getNewsBySlug(params.slug);
@@ -9,4 +9,35 @@ export function GET(req, { params }) {
             { status: 404 });
     }
     return Response.json(news);
+}
+
+export async function PATCH(req, { params }) {
+    const body = await req.json();
+    const allowedFields = ["title", "description"];
+
+    const invalidField = Object.keys(body).find(
+        key => !allowedFields.includes(key)
+    );
+
+    if (invalidField) {
+        return Response.json(
+            { error: `You can only update title or description` },
+            { status: 400 }
+        );
+    }
+
+    const news = getAllNews();
+    const index = news.findIndex(n => n.slug === params.slug);
+
+    if (index === -1) {
+        return Response.json(
+            { message: "News not found" },
+            { status: 404 }
+        );
+    }
+
+    news[index] = { ...news[index], ...body };
+    saveNews(news);
+
+    return Response.json(news[index]);
 }
